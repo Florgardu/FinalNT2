0e00dbfce936cc0a7e35445cd1e9ad487ad16ba4
FlorGardu <garduno.florencia@gmail.com>
2020-12-01 20:56:56 -0300

    subo final

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -51,44 +51,18 @@ export default {
 
   },
   computed: {
-contarPalabras(){
-let palabras = 0;
-if(this.texto){
-palabras = this.texto.trim().split(/\s+/).length;
-}
-return palabras;
-}
-}
+  contarPalabras(){
+  let palabras = 0;
+  if(this.texto){
+  palabras = this.texto.trim().split(/\s+/).length;
+  }
+  return palabras;
+  }
+  }
 }
 
 </script>
 
 <style scoped lang="css">
-
-  .jumbotron {
-    background-color: rgb(25, 184, 144);
-    color: white;
-  }
-
-  hr {
-    background-color: #ddd;
-  }
-
-  .btn {
-    border: none;
-    background-color: white;
-    font-family: "Montserrat", "Avenir";
-    text-transform: uppercase;
-    height: 100%;
-    font-weight: 700;
-    letter-spacing: 1px;
-    color: steelblue;
-    transition: all 0.3s;
-    outline: none;
-  }
-  .btn:hover {
-    color: white;
-    background-color: rgb(216, 95, 15);
-  }
 
 </style>
